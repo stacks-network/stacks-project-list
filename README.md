cb2264c54d6826528e42b806f8a42b7945d95f50
Alexander Graebe <alex@blockstack.com>
2020-08-13 07:51:32 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # stacks-community-projects
  A list of community projects in the Stacks ecosystem
+
+## Install
+
+```shell
+pip install requirements.txt
+```
+
+## Set token
+Before running the script, in the `enrich.py` file, you will need to set your own [GitHub personal access token](https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token).
+
+## Generate enriched list
+The Python script is used to run through a CSV file of relevant Stacks repos and enrich them using the GitHub API.
+
+```shell
+python enrich.py
+```
+
+## Contributions
+If you would like your repo to be part of the list, please submit a PR with a new line in the `github-repos.csv` file. The format is `<user>/<repo>`. For instance:
+
+```csv
+agraebe/stacks-community-projects
+```
---
+++ enrich.py
@@ -0,0 +1,51 @@
+import csv
+from github import Github
+
+token = "<GITHUB_ACCESS_TOKEN>"
+g = Github(token)
+repos = []
+
+with open("github-repos.csv") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row["Repository"])
+        try:
+            repo = g.get_repo(row["Repository"])
+            repos.append(repo)
+        except:
+            print("Could not find GitHub repository")
+
+with open("github-repos-enriched.csv", mode="w") as csv_file:
+    fieldnames = [
+        "url",
+        "name",
+        "description",
+        "homepage",
+        "forks",
+        "stars",
+        "issues",
+        "network_count",
+        "subscribers_count",
+        "watchers_count",
+        "created_at",
+    ]
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter=",")
+
+    writer.writeheader()
+    for repo in repos:
+
+        writer.writerow(
+            {
+                "url": repo.url,
+                "name": repo.name,
+                "description": repo.description,
+                "homepage": repo.homepage,
+                "forks": repo.forks_count,
+                "stars": repo.stargazers_count,
+                "network_count": repo.network_count,
+                "subscribers_count": repo.subscribers_count,
+                "watchers_count": repo.watchers_count,
+                "created_at": repo.created_at,
+            }
+        )
+
